bc3438972dc4dc167f14f07144c8dc64a8d683b1
Alvin Galit <aroblesgalit@gmail.com>
2020-06-17 15:22:11 -0500

    Created actions for increment an decrement

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+
 
 
 // STORE -> GLOBALIZED STATE -- holds all data/state for your app 
 
 // ACTION INCREMENT -- name that describes what you want to do
+const increment = () => {
+  return {
+    type: "INCREMENT"
+  }
+}
+const decrement = () => {
+  return {
+    type: "DECREMENT"
+  }
+}
+
 
 // REDUCER -- describes how your action (name) transforms your current state into a new state
 
