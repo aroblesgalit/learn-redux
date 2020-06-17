5e8ee6f56fab1320f7899a24d5f926a518b09897
Alvin Galit <aroblesgalit@gmail.com>
2020-06-17 16:26:23 -0500

    Updates

---
+++ precache-manifest.957c86f42282546719a0da583f00cf89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e8ab8040b75c8939d8416ff4d870020",
+    "url": "/learn-redux/index.html"
+  },
+  {
+    "revision": "ab4a52db179c94935458",
+    "url": "/learn-redux/static/css/main.d434ba05.chunk.css"
+  },
+  {
+    "revision": "197a989d1bd53fdd73e1",
+    "url": "/learn-redux/static/js/2.8ea76c4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learn-redux/static/js/2.8ea76c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab4a52db179c94935458",
+    "url": "/learn-redux/static/js/main.97980284.chunk.js"
+  },
+  {
+    "revision": "6a870a4d30c0034d77c6",
+    "url": "/learn-redux/static/js/runtime-main.cfc05697.js"
+  }
+]);
