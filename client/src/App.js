edb5216ba83f98787bb3c205076c7d1423035ed7
Alvin Galit <aroblesgalit@gmail.com>
2020-06-17 15:55:23 -0500

    Used useSelector to access store and its state

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button>+</button>
+      <button>-</button>
+      {
+        isLogged ?
+          <h3>Valuable Information I shouldn't see</h3> :
+          ""
+      }
+
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from "redux";
 import allReducers from "./reducers";
+import { Provider } from "react-redux";
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
 // // STORE -> GLOBALIZED STATE -- holds all data/state for your app 
@@ -45,7 +49,12 @@ const store = createStore(allReducers);
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
