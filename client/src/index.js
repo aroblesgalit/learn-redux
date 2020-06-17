e45678a540a1d2817e754c34876bf158f1e01f44
Alvin Galit <aroblesgalit@gmail.com>
2020-06-17 15:34:10 -0500

    Created a counterReducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,36 +3,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from "redux";
+// import { createStore } from "redux";
 
 
 
-// STORE -> GLOBALIZED STATE -- holds all data/state for your app 
+// // STORE -> GLOBALIZED STATE -- holds all data/state for your app 
 
-// ACTION INCREMENT -- name that describes what you want to do
-const increment = () => {
-  return {
-    type: "INCREMENT"
-  }
-}
-const decrement = () => {
-  return {
-    type: "DECREMENT"
-  }
-}
+// // ACTION INCREMENT -- name that describes what you want to do
+// const increment = () => {
+//   return {
+//     type: "INCREMENT"
+//   }
+// }
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT"
+//   }
+// }
 
-// REDUCER -- describes how your action (name) transforms your current state into a new state
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  }
-}
+// // REDUCER -- describes how your action (name) transforms your current state into a new state
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// }
 
+// let store = createStore(counter);
 
-// DISPATCH -- executes the action (name)
+// // Display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH -- executes the action (name)
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 
 
